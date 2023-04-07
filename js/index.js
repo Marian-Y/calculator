50f2a9552d0d3d130f8cb0f9b96378e4fafe301d
Marian-Y <marianyatsynyk@ukr.net>
2023-04-08 01:08:11 +0300

    The calculator is almost ready

--- js/index.js
+++ js/index.js
@@ -1,9 +1,192 @@
-const main = function (){
-    alert(`Привіт світ`);
-};
+var input = document.getElementById('input');
+var error = document.getElementById('error');
+
+let noFirstNumber = 399,
+
+    wrongNumberError = 404,
+
+    numberZero = 0;
+
+
+function number(button){
+  // input.innerHTML += button.innerHTML
+  inputString = input.innerHTML;
+  var lastChar = inputString[inputString.length - 1];
+  var almostLastChar = inputString[inputString.length - 2];
+
+  var last = almostLastChar + lastChar
+  
+  console.log(`inputString`, inputString);
+  console.log(`lastChar`, last);
+
+  switch (true){
+    case [`/0`,`*0`,`-0`,`+0`].includes(last):
+      console.log(`case 1`)
+      newNumb = inputString.substring(0, inputString.length - 1) + button.innerHTML;
+      input.innerHTML = newNumb;
+    break
+      // case inputString.length == 0: 
+      // console.log(`case 2`);
+      // throw new Error(noFirstNumber);
+    default:
+        input.innerHTML += button.innerHTML;
+        error.innerHTML = ``;
+        console.log(`DEFAULT`)
+  } 
+  
+}
+
+function operator(button){
+
+  // Що не так з var в при викалика у другу функції
+
+  inputString = input.innerHTML;
+  console.log(inputString);
+  var lastChar = inputString[inputString.length - 1];
+  console.log(`lastChar`, lastChar);
+
+  switch (true){
+    case [`+`,`-`,`*`,`/`].includes(lastChar):
+      console.log(`case 1`)
+      newOper = inputString.substring(0, inputString.length - 1) + button.innerHTML;
+      input.innerHTML = newOper;
+    break
+    case inputString.length == 0: 
+      error.innerHTML = `Оператор не може бути першим`;
+      myMove()
+      throw new Error(noFirstNumber);
+    default:
+      input.innerHTML += button.innerHTML;
+      console.log(`DEFAULT`)
+  }
+
+}
+
+
+function calculate(){  
+  var miracle = input.innerHTML;
+  
+  let separation = miracle.split(/(\+|\-|\*|\/)/);
+  let number = [];
+  let operator = [];
+
+  for (var i = 0; i < separation.length; i++) {
+    if(i % 2 === 0) {
+      number.push(separation[i]);
+    } else {
+      operator.push(separation[i]);
+    }
+  }
+  
+  console.log(number)
+  console.log(operator)
+
+  var divide = operator.indexOf(`/`)
+  var multiply = operator.indexOf(`*`)
+  var minus = operator.indexOf(`-`)
+  var plus = operator.indexOf(`+`)
+      
+  while (divide != -1) {
+    number.splice(divide, 2, result = Number(number[divide]) / Number(number[divide + 1]));
+    operator.splice(divide, 1);
+    divide = operator.indexOf("/");
+  }
+  while (multiply != -1) {
+    number.splice(multiply, 2, result = Number(number[multiply]) * Number(number[multiply + 1]));
+    operator.splice(multiply, 1);
+    multiply = operator.indexOf("*");
+  }
+  while (minus != -1) {
+    number.splice(minus, 2, result = Number(number[minus]) - Number(number[minus + 1]));
+    operator.splice(minus, 1);
+    minus = operator.indexOf("-");
+  }
+  while (plus != -1) {
+    number.splice(plus, 2, result = Number(number[plus]) + Number(number[plus + 1]));
+    operator.splice(plus, 1);
+    plus = operator.indexOf("+");
+  }
+
+  input.innerHTML = ``;
+  input.innerHTML += result ;
+}
+
+function clr(){
+  input.innerHTML = ``;
+
+}
 
 
 
-// git add .
-// git commit -m "fix coments"
-// git push
+const newspaper = document.getElementById("error");
+
+function myMove() {
+
+  const newspaperSpinning = [
+    { color: `yellow`,
+      transform: `scale(0)`,
+      opacity: `0`,
+     },
+    { color: `red`,
+      transform: `scale(2)`,
+      opacity: `1`, },      
+    { color: `yellow`,
+      transform: `scale(1)`,
+      opacity: `1`, },
+    { transform: `scale(1)`,
+      opacity: `1`, },
+  ];
+  
+  const newspaperTiming = {
+    duration: 2000,
+    iterations: 1,
+  };
+  
+
+  // newspaper.addEventListener("click", () => {
+    newspaper.animate(newspaperSpinning, newspaperTiming);
+  // });
+
+
+}
+
+try {
+  calculate()
+} catch (error) {
+  switch(true){
+    case (error.message == noFirstNumber):
+      
+  }
+}
+
+// let inputString = `123455`
+// let six = `6`;
+
+// let sub = inputString.subinputString(0, inputString.length - 1) + six;
+
+// console.log(inputString);
+// console.log(sub);
+
+// let miracle = `11+22-33/44*555-55`
+
+
+// let sort = da.forEach(function(value) {
+  
+// });
+
+// let filters = [`-`,`+`,`/`,`*`]
+// let ar = arr.split(/\+|\-|\*|\=|\//g);
+// let a = arr
+//   .split(/[0-9]/g)
+//   .forEach((value) =>{
+//     console.log(value)
+//   });
+// // let o = a
+
+// console.log(`arr`, arr);
+// console.log(`ar`,ar);
+// console.log(`a`,a);
+// console.log(arr);
+// console.log(o);
+
+
